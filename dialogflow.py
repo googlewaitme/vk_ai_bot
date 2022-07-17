1869a0cb237d3b26d3214cf6a1150c7fd634c82a
Bulat <kusok.apelbsina@gmail.com>
2022-07-17 13:33:28 +0300

    feature/5: added checking user is adult

--- dialogflow.py
+++ dialogflow.py
@@ -1,10 +1,12 @@
 from google.cloud import dialogflow_v2beta1 as dialogflow
 from google.protobuf.json_format import MessageToDict
 
-from datetime import date
+from datetime import date, timedelta
+
 
 def get_digits_from_string(string):
     return ''.join([n for n in string if n.isdigit()])
+
 
 class DialogFlow:
     def __init__(self):
@@ -27,6 +29,11 @@ class DialogFlow:
         params = response.query_result.parameters
         intent_name = response.query_result.intent.display_name
         is_all_params = response.query_result.all_required_params_present
+        if intent_name == 'KreditForm' and params['birthday']:
+            if not self._is_adult(params.pb):
+                return 'По законодательству РФ мы не в праве заключать ' \
+                       'договора с гражданами не достигшими возраста ' \
+                       '18 лет. Обратитесь позже.'
         if intent_name == 'KreditForm' and is_all_params:
             url = self.make_url(params.pb)
             answer += "\n\n" + url
@@ -52,6 +59,14 @@ class DialogFlow:
         amount = get_digits_from_string(params['SumForLoan'].string_value)
         url += f"sum={amount}"
         return url
+
+    def _is_adult(self, params):
+        birthday_string = MessageToDict(params['birthday'])
+        birthday_date = self._get_date(birthday_string)
+        today = date.today()
+        age = today - birthday_date
+        eightteen = timedelta(days=364 * 18)
+        return eightteen < age
 
     def _get_date(self, string):
         # parse strings like 2022-04-05T00:00:00+05:00
